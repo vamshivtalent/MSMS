0dca2df7d00dac216c0fb3339411c968921afa51
Krishna G <krishna.vtalent3@gmail.com>
2019-09-05 15:24:15 +0530

    BAL added

---
+++ MSMS/MSMS/Areas/Admin/BAL/BusinessAccessLayyer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MSMS.Areas.Admin.BAL
+{
+    public class BusinessAccessLayyer
+    {
+    }
+}
